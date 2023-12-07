63884c222ba3a8756c7e58a4e2972642084427f7
Amirul Asraf <amirul.asraf9135@gmail.com>
2023-12-07 16:30:24 +0800

    nav menu

--- client/src/components/NavMenu.tsx
+++ client/src/components/NavMenu.tsx
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
-import './NavMenu.scss'
+import './NavMenu.scss' 
 
 const NavMenu: React.FC = () => {
     const [isVisible, setIsVisible] = useState(true);
-    const handleToggle = (shouldShow:boolean) => {
+    const handleToggle = (shouldShow: boolean) => {
         setIsVisible(shouldShow);
     };
     return (
         <div className='menu-base'>
-
-            <div className={`menu ${isVisible?'show':''}`} onMouseEnter={()=>handleToggle(true)} onMouseLeave={()=>handleToggle(false)}>
-                <div className='icon'>
-                    M
+            <div className='menu'>
+                <div className='menu-icon'>
+                <i className="fa fa-bars"></i>
                 </div>
-                
-<div className='items-container'>
-                    <div className='items'>A</div>
-                    <div className='items'>B</div>
-                    <div className='items'>C</div>
-                    <div className='items'>A</div>
-                    <div className='items'>B</div>
-                    <div className='items'>C</div>
+                <div className='menu-border'>
+
+                </div>
+                <div className='menu-content'>
+                    <div className='link'>
+                        Satu
+                    </div>
+                    <div className='link'>
+                        Satu
+                    </div>
+                    <div className='link'>
+                        Satu
+                    </div>
+                    <div className='link'>
+                        Satu
+                    </div>
                 </div>
             </div>
         </div>
