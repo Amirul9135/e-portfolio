a4133b225ad7f885fdd242a01be62beb7e467b42
Amirul Asraf <amirul.asraf9135@gmail.com>
2023-12-10 11:51:30 +0800

    about

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -80,20 +80,22 @@ const About: React.FC = () => {
           <div className='sect-title'><h2>My Journey<div><i className="fa fa-share-alt-square glowIcon" aria-hidden="true"><i className="fa fa-share-alt-square" aria-hidden="true"></i></i></div></h2></div>
           <ul className="timeline">
             <li>
-              <p className='timerange'>1999 - 2011</p>
-              <p>Born in December 1999 I was raised in my hometown Kampung Bukit Kuching, Selangor. I don’t really have particular goal back then, going to my primary school only because it is normal thing to do and encouraged by my family like most kids do. But after my UPSR which was my first big examination, I managed to pass with flying colors and got straight A, it was life changing experience seeing how happy my family especially my mom was. It motivates me even more to study harder.</p>
+              <div className='timerange'><i className="fa fa-circle-o-notch glowIcon" aria-hidden="true"><i className="fa fa-circle-o-notch" aria-hidden="true"></i></i><p>1999 - 2011</p></div>
+              <p className='desc'>Born in December 1999 I was raised in my hometown Kampung Bukit Kuching, Selangor. I don’t really have particular goal back then, going to my primary school only because it is normal thing to do and encouraged by my family like most kids do. But after my UPSR which was my first big examination, I managed to pass with flying colors and got straight A, it was life changing experience seeing how happy my family especially my mom was. It motivates me even more to study harder.</p>
             </li>
             <li>
-              <p className='timerange'>2012 - 2016</p>
-              <p>Throughout my highschool days I started to grow closer toward computers. Starting from simply gaming (quite a lot) onmy brothers' laptop, around 15 years old I have access to my own laptop which allows me to explore more. I also participated in the Iltizam team which responsible in designing my school yearbooks. The first thing closest to programming that I tried was creating custom maps for Warcraft 3 for fun. As I play more game, my curiosity also grew at some point I started to create bots to automate gameplay using actual programming language</p>
+              <div className='timerange'><i className="fa fa-circle-o-notch glowIcon" aria-hidden="true"><i className="fa fa-circle-o-notch" aria-hidden="true"></i></i><p>2012 - 2016</p></div>
+               <p className='desc'>Throughout my highschool days I started to grow closer toward computers. Starting from simply gaming (quite a lot) onmy brothers' laptop, around 15 years old I have access to my own laptop which allows me to explore more. I also participated in the Iltizam team which responsible in designing my school yearbooks. The first thing closest to programming that I tried was creating custom maps for Warcraft 3 for fun. As I play more game, my curiosity also grew at some point I started to create bots to automate gameplay using actual programming language</p>
             </li>
             <li>
-              <p className='timerange'>2017 - 2020</p>
-              <p>Since I spent a lot of time with computer and also likes logical things such as programming, math etc. I decided to pursue my passion in IT by enrolling to UTHM in Johor for my diploma at 2017. It was a very new experience to me as it was my first time living away from my family but it was very statisfying and throughout my studies I was really glad for choosing to further my study in this IT field which have limitless potential.</p>
+             
+            <div className='timerange'><i className="fa fa-circle-o-notch glowIcon" aria-hidden="true"><i className="fa fa-circle-o-notch" aria-hidden="true"></i></i><p>2017 - 2020</p></div>
+              <p className='desc'>Since I spent a lot of time with computer and also likes logical things such as programming, math etc. I decided to pursue my passion in IT by enrolling to UTHM in Johor for my diploma at 2017. It was a very new experience to me as it was my first time living away from my family but it was very statisfying and throughout my studies I was really glad for choosing to further my study in this IT field which have limitless potential.</p>
             </li>
             <li>
-              <p className='timerange'>2021 - Now</p>
-              <p>My original plan was to continue my study in 2020 right after graduating from diploma which was delayed due to the global pandemic. At september 2021 I enrolled to UTEM at Melaka to further my study in Computer Science specializing in Software Development until today. I met lot of friends and great lecturer at UTEM. During my third year of study, I was blessed with the oppoturnity to work as a part time programmer at the UTEM information office.</p>
+             
+            <div className='timerange'><i className="fa fa-circle-o-notch glowIcon" aria-hidden="true"><i className="fa fa-circle-o-notch" aria-hidden="true"></i></i><p>2021 - Now</p></div>
+              <p className='desc'>My original plan was to continue my study in 2020 right after graduating from diploma which was delayed due to the global pandemic. At september 2021 I enrolled to UTEM at Melaka to further my study in Computer Science specializing in Software Development until today. I met lot of friends and great lecturer at UTEM. During my third year of study, I was blessed with the oppoturnity to work as a part time programmer at the UTEM information office.</p>
             </li>
           </ul>
 
