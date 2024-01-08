9aaa68e0a7db3ad7d6f333bba19cc4fa429d3a3d
Amirul Asraf <amirul.asraf9135@gmail.com>
2024-01-08 16:19:13 +0800

    resume view

--- client/src/components/Resume.tsx
+++ client/src/components/Resume.tsx
@@ -5,16 +5,15 @@ import { Viewer, Worker } from "@react-pdf-viewer/core";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-
-
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";    
+import resumepdf from "../images/Resume.pdf"
 
 const Resume: React.FC = () => {  
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  return (
+  return ( 
     <React.Fragment> 
         
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js">
+    <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js'>
       <div
         style={{
           height: "100%",
@@ -24,7 +23,7 @@ const Resume: React.FC = () => {
         }}
       >
         <Viewer
-          fileUrl="/download/resume"
+          fileUrl={`${resumepdf}`}
           plugins={[defaultLayoutPluginInstance]}
         />
       </div>
